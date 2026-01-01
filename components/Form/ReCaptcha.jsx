a259c4baef7690f86f1010a3ee9c47151e0bdb69
faysal725 <faysal.dev@gmail.com>
2026-01-01 12:06:45 +0600

    recaptcha key updated

--- components/Form/ReCaptcha.jsx
+++ components/Form/ReCaptcha.jsx
@@ -4,12 +4,14 @@ import { getRecaptchaKey } from '@/lib/exportVariables';
 
 
 export default function ReCaptcha() {
+
+    const recaptchaKey = getRecaptchaKey()
     const handleRecaptchaChange = (value) => {
         console.log("Captcha token:", value);
         // you can send `value` (the token) to your server to verify
     };
     return (
-        <ReCAPTCHA sitekey={getRecaptchaKey}
+        <ReCAPTCHA sitekey={recaptchaKey}
             onChange={handleRecaptchaChange}
         />
     )
